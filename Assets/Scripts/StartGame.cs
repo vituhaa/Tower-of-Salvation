37dcff354c33206017657be3ef6e493b1d965228
vituhaa <vbuzina72@gmail.com>
2026-06-15 20:35:06 +0300

    3d level added, first scene added

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    [SerializeField] private string firstLevelName = "Level_1";
+
+    public void Start_Game()
+    {
+        PlayerPrefs.DeleteKey("LastLevel");
+        SceneManager.LoadScene(firstLevelName);
+    }
+
+    public void ContinueGame()
+    {
+        if (PlayerPrefs.HasKey("LastLevel"))
+        {
+            string lastLevel = PlayerPrefs.GetString("LastLevel");
+            SceneManager.LoadScene(lastLevel);
+        }
+        else
+        {
+            Start_Game();
+        }
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
